6a0c9d91b6a4178970a1b42201be0b9fbbdb38b0
kfrrsf <110762586+kfrrsf@users.noreply.github.com>
2022-08-07 16:27:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ if os.path.exists("local.env"):
 
 load_dotenv()
 admins = {}
-SESSION_NAME = getenv("SESSION_NAME", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+SESSION_NAME = getenv("SESSION_NAME", "AgDEaPROSsNvqguHyxXkNzjV4g9OmdNtTFTCrqXv6blgUk3dGm5AXN7VFiDatlWqyxhw019W307IiMrL6D_j_RYt3O659t-sx_gRQyPKZ9tixkXEuzJddCCZeCmc7rDK-wWB-eU37qd9qO-ywrV_QnTh7aeIz-dmY47RnOl2_mhuMowFNwOk19XrUSmmRV8eAepwFsgSFOHlfK-2JD8QD9aZqLIzkQ8ky9DTFrH8m7xajyQNNiXbuv16WJuEJfVKevT0PuWODgzLupykvyj1BNZ97ex3beW6rkkb2ky2lPq51lt1V-i1su-vhPGJPw1vSUv3943RcvsBk74R-cSr5pI4AAAAAUtZP_UA")
+BOT_TOKEN = getenv("BOT_TOKEN", "5516645654:AAFECMgI71LCCdhFm6IcGm_GZRsQgGDCMB4")
 BOT_NAME = getenv("BOT_NAME", "song")
-API_ID = int(getenv("API_ID", "8186557"))
-API_HASH = getenv("API_HASH", "efd77b34c69c164ce158037ff5a0d117")
+API_ID = int(getenv("API_ID", "1975516"))
+API_HASH = getenv("API_HASH", "419c5fbaab800b44c89e436920ca0455")
 OWNER_NAME = getenv("OWNER_NAME", "muntazer")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "ZzZzZ5")
 ALIVE_NAME = getenv("ALIVE_NAME", "muntazer")
